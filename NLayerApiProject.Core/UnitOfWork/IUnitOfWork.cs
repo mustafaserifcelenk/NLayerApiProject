6012634f1578c4d3bdd451204da3609eee388c4f
Mustafa <mustafaserifcelenk@gmail.com>
2021-04-08 21:14:14 +0300

    Create IUnitOfWork

---
+++ NLayerApiProject.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using NLayerApiProject.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApiProject.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        // Bunları dependency injection olarak verebilirdik bu daha bestpractice
+        IProductRepository Product { get; }
+        ICategoryRepository Category { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
