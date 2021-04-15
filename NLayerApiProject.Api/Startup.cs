e56b393b8df7e2771e4da68abd5cd6bbf8b953d3
Mustafa <mustafaserifcelenk@gmail.com>
2021-04-15 12:54:38 +0300

    Add DbContext and IUnitOfWork to Startup.cs

--- NLayerApiProject.Api/Startup.cs
+++ NLayerApiProject.Api/Startup.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NLayerApiProject.Core.UnitOfWork;
+using NLayerApiProject.Data;
+using NLayerApiProject.Data.UnitOfWorks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +30,11 @@ namespace NLayerApiProject.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConStr"].ToString());
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
