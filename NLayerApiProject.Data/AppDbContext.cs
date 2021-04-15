751feeeba56322b849e18421e6d5f6ab3d6905d2
Mustafa <mustafaserifcelenk@gmail.com>
2021-04-15 10:42:09 +0300

    Create AppDbContext

---
+++ NLayerApiProject.Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerApiProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApiProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Options seçeneği ne kullanılacağını belirtiyor(sqlserver, postgre vs.)
+        // Api veya Web'in startup'ında bu option doldurulacak
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
