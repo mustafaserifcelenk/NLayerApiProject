6a20a1f368f540d22761746561cb5c39a7cc0153
Mustafa <mustafaserifcelenk@gmail.com>
2021-04-15 12:46:36 +0300

    Create UnitOfWork.cs

---
+++ NLayerApiProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using NLayerApiProject.Core.Repository;
+using NLayerApiProject.Core.UnitOfWork;
+using NLayerApiProject.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApiProject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IProductRepository Product => _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Category => _categoryRepository = _categoryRepository ?? new CategoryRepository(_context);
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
