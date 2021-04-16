43f25e0eb5314ed393103b00e3d796cd37413810
Mustafa <mustafaserifcelenk@gmail.com>
2021-04-16 16:06:41 +0300

    Create CategoryController & DTOs

---
+++ NLayerApiProject.Api/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerApiProject.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerApiProject.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            // Dependency injection burada ICategoryService ile karşılaştı ve gitti categoryService.cs alıp burada nesneye atadı
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return Ok(categories);
+        }
+    }
+}
