fe774f6e7df296f7dd8a123f476e4ca55b2cb8e3
Mustafa <mustafaserifcelenk@gmail.com>
2021-04-17 17:41:08 +0300

    Create PersonController

---
+++ NLayerApiProject.Api/Controllers/PeopleController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerApiProject.Core.Models;
+using NLayerApiProject.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerApiProject.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly IService<Person> _personService;
+
+        public PeopleController(IService<Person> service)
+        {
+            _personService = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _personService.GetAllAsync();
+            return Ok(persons);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Person person)
+
+        {
+            var newperson = await _personService.AddAsync(person);
+
+            return Ok(newperson);
+        }
+    }
+}
